34f1a928fa12bdc464c2e4fcc17cfe36648ad62b
TuanPM28 <tuanpham22790@gmail.com>
2025-03-21 23:44:01 +0700

    navbar and sidebar

--- src/components/navbar-component/NavbarComponent.tsx
+++ src/components/navbar-component/NavbarComponent.tsx
@@ -1,10 +1,13 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
 import "./NavbarComponent.scss";
 import Magnetic from "../magnetic/Magnetic";
 import { forwardRef } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Offcanvas from "react-bootstrap/Offcanvas";
 
 interface NavbarProps {}
 
@@ -12,16 +15,37 @@ const NavbarComponent = forwardRef<HTMLDivElement, NavbarProps>(
   function NavbarComponent(props, ref) {
     return (
       <>
-        <Navbar expand="lg" className="navbar">
+        <Navbar expand={false} className="navbar">
           <Navbar.Brand>
             <Link to="/">{"</> Tuan"}</Link>
           </Navbar.Brand>
 
-          <Magnetic>
-            <div className="burger">
-              <div ref={ref} className="bounds"></div>
-            </div>
-          </Magnetic>
+          <Navbar.Toggle aria-controls={"offcanvasNavbar-expand-false"}>
+            <Magnetic>
+              <div className="burger">
+                <div ref={ref} className="bounds"></div>
+              </div>
+            </Magnetic>
+          </Navbar.Toggle>
+
+          <Navbar.Offcanvas
+            id={`offcanvasNavbar-expand-false`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-false`}
+            placement="end"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-false`}>
+                {"</> Tuan"}
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/about-me">About me</Nav.Link>
+                <Nav.Link href="/work">My work</Nav.Link>
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
         </Navbar>
       </>
     );
